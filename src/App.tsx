47524486199adbc438e4b93468fab40d3fc87673
konovalM <konovalenko.ii4elka@mail.ru>
2022-12-10 10:30:53 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+import {Button} from "react-bootstrap";
+import StudentCard from "./components/cards/studentCard/StudentCard";
+
+function App() {
+    return (
+        <div className="App">
+            1234
+            <Button>
+                Test
+            </Button>
+            <StudentCard />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/cards/studentCard/StudentCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './StudentCard.module.scss'
+
+const StudentCard = () => {
+    return (
+        <div className={styles.wrapper}>
+
+        </div>
+    );
+};
+
+export default StudentCard;
